14a144c9d38ffacd50845e8a4f231ca8d3d080c5
Khodazone <khodazone@gmail.com>
2023-04-07 10:32:25 +0200

    dati del lavoro

---
+++ script.js
@@ -0,0 +1,16 @@
+// ----- COMPONENTE JS DI INTERAZIONE CON L'UTENTE -----
+
+// Dati richiesti 
+
+let typeOfWork = document.getElementById("exampleFormControlInput5").value;
+
+if (typeOfWork == "backend"){
+    price = 20.50;
+} 
+else if (typeOfWork == "frontend"){
+    price = 15.30;
+} 
+else if (typeOfWork == "analysis"){
+    price = 33.60;
+}
+
