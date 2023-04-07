4ea5dd2b8f7bb68d23fb8a27ee3a186e61b3065f
Khodazone <khodazone@gmail.com>
2023-04-07 13:00:27 +0200

    funzioni

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // ----- COMPONENTE JS DI INTERAZIONE CON L'UTENTE -----
 
+
+// Codici Sconto per un 25% di sconto sul prezzo finale
+let arrayDiscountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+
 // FUNZIONI DEL FORM
 function calculateMyPrice(event){
     event.preventDefault();
@@ -25,10 +30,8 @@ function calculateMyPrice(event){
         document.getElementById("errorHours").innerHTML = "ERROR: Enter a correct number!";
     }
 
-    // Codici Sconto per un 25% di sconto sul prezzo finale
-    let arrayDiscountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
     let inputDiscount = document.getElementById("exampleFormControlInput6").value;
-    let codeInArray;
+    let codeInArray = false;
 
     // Codice sconto nell'array
     for (let i = 0; i < arrayDiscountCodes.length; i++){
@@ -42,18 +45,28 @@ function calculateMyPrice(event){
 
     if (!codeInArray){
         document.getElementById("msg-1").innerHTML = "Invalid Discount Code!";
-        document.getElementById("msg-2").innerHTML = "Price without discount: " + "x" + "&euro;";
+        document.getElementById("msg-2").innerHTML = "Price without discount: " + priceWithoutDiscount(hoursRequested, price) + "&euro;";
     } else {
         document.getElementById("msg-1").innerHTML = "Discount Code = 25%";
-        document.getElementById("msg-2").innerHTML = "Price with a discount: " + "y" + "&euro;";
+        document.getElementById("msg-2").innerHTML = "Price with a discount: " + priceWithDiscount(arrayDiscountCodes, inputDiscount, (priceWithoutDiscount(hoursRequested, price))) + "&euro;";
     }
 }
 
 
-
-
-
-
-
 // ALTRE FUNZIONI
+
+function priceWithDiscount(arrayDiscount, discountInserted, result){
+    for (let i = 0; i < arrayDiscount.length; i++){
+        if (discountInserted == arrayDiscount[i]){
+            let percent = result * 0.25;
+            let discount = result - percent;
+            return discount.toFixed(2);
+        }       
+    }
+}
+
+function priceWithoutDiscount (hoursRequested, price){
+    let result = hoursRequested * price;
+    return result.toFixed(2);
+}
 
